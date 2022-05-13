b830c058b57798e14dc2421115d4e9d0f188d2ad
GrealishCalves <maximmilshtien@gmail.com>
2022-05-13 12:37:36 +0300

    manipulate correct guess css

--- Section-6/script.js
+++ Section-6/script.js
@@ -4,26 +4,37 @@ let stateScore = 20;
 
 document.querySelector(".check").addEventListener("click", function () {
 	const guess = Number(document.querySelector(".guess").value);
-	console.log(guess, typeof guess);
+
+	// when there no input
 	if (!guess) {
-		document.querySelector(".message").textContent = "Please enter a guess";
+		document.querySelector(".message").textContent = "no guess";
+
+		// when there is a guess
 	} else if (guess === secretKey) {
 		document.querySelector(".message").textContent = "Correct guess";
+
+		document.querySelector("body").style.backgroundColor = "#60b347";
+		document.querySelector(".number").style.width = "30rem";
+
+		// when guess is too high
 	} else if (guess > secretKey) {
-		document.querySelector(".score").textContent = stateScore--;
 		if (stateScore > 1) {
+			stateScore--;
+			document.querySelector(".score").textContent = stateScore;
 			document.querySelector(".message").textContent = "Too high guess";
 		} else {
-			document.querySelector(".message").textContent = "you lost the game";
 			document.querySelector(".score").textContent = 0;
+			document.querySelector(".message").textContent = "you lost the game";
 		}
+		// when guess is too low
 	} else if (guess < secretKey) {
-		document.querySelector(".score").textContent = stateScore--;
 		if (stateScore > 1) {
+			stateScore--;
+			document.querySelector(".score").textContent = stateScore;
 			document.querySelector(".message").textContent = "Too low guess";
 		} else {
-			document.querySelector(".message").textContent = "you lost the game";
 			document.querySelector(".score").textContent = 0;
+			document.querySelector(".message").textContent = "you lost the game";
 		}
 	}
 });
