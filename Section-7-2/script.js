80902afdc9ff48921ca79ef1ca8b60c816d154a2
GrealishCalves <maximmilshtien@gmail.com>
2022-05-14 13:09:34 +0300

    define pop-up close and open

--- Section-7-2/script.js
+++ Section-7-2/script.js
@@ -1,1 +1,28 @@
 "use strict";
+
+//VAR:
+const modal = document.querySelector(".modal");
+const btnOpenModal = document.querySelectorAll(".show-modal");
+const btnCloseModal = document.querySelector(".close-modal");
+const overlay = document.querySelector(".overlay");
+
+//FUNCTION: define an open modal function
+const openModal = function () {
+	modal.classList.remove("hidden");
+	overlay.classList.remove("hidden");
+};
+
+//FUNCTION: define an close modal function
+const closeModal = function () {
+	modal.classList.add("hidden");
+	overlay.classList.add("hidden");
+};
+
+//FUNCTION: select each button and apply the open function
+for (let i = 0; i < btnOpenModal.length; i++) {
+	btnOpenModal[i].addEventListener("click", openModal);
+}
+
+//FUNCTION: define actions to close the modal
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
