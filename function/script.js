a3c668311514c0430351283e1839a8ac600aff62
GrealishCalves <maximmilshtien@gmail.com>
2022-06-14 19:58:56 +0300

    .

--- function/script.js
+++ function/script.js
@@ -1,38 +1,44 @@
+// HINT: Passing by value and reference
+// javascript doesn't work with passing by reference
+
 // the function createBooking is called with the parameters flightNum and numPassenger setting the default value of numPassenger to 1 and price to 200
+// const bookings = [];
+// const createBooking = (
+// 	flightNum,
+// 	numPassenger = 1,
+// 	price = 200 * numPassenger
+// ) => {
+// 	const booking = {
+// 		flightNum,
+// 		numPassenger,
+// 		price,
+// 	};
+// 	console.log(booking);
+// 	bookings.push(booking);
+// };
+// createBooking("LV123", 2, 100);
+// createBooking("LV123", 2);
+// createBooking("LV123", undefined, 100);
 
-const bookings = [];
-const createBooking = (
-	flightNum,
-	numPassenger = 1,
-	price = 200 * numPassenger
-) => {
-	const booking = {
-		flightNum,
-		numPassenger,
-		price,
-	};
-	console.log(booking);
-	bookings.push(booking);
+const object = {
+	name: "John duane",
+	passport: 12345,
 };
-createBooking("LV123", 2, 100);
-createBooking("LV123", 2);
-createBooking("LV123", undefined, 100);
-
-const flightNum = "LD321";
-const passenger = {
-	name: "John",
-	passport: "12345",
-};
+const flight = "LD321";
+console.log(object);
+console.log(flight);
 
 // passing arguments to the function, value vs reference
 
-const checkInflight = (flightNum, Passenger) => {
-	flightNum = "LV123"; // primitive value -  will be changed to the value of the variable flightNum
-	Passenger.name = "Mr " + Passenger.name; // reference value - will be changed to the original object Passenger
+const checkInflight = function (flightNum, passenger) {
+	flightNum = "LV123"; // primitive value
+	passenger.name = "Mr " + passenger.name; // passing a reference to the function.
 
-	if (passenger.passport === "12345") {
-		console.log("check in success");
-	} else console.log("Sorry, you are not allowed to enter");
+	if (passenger.passport === 12345) {
+		console.log("Welcome " + passenger.name);
+	} else {
+		console.log("You are not allowed to enter");
+	}
 };
 
 const newPassport = (passenger) => {
@@ -40,11 +46,12 @@ const newPassport = (passenger) => {
 		Math.random().toString(36).substring(2, 15) +
 		Math.random().toString(36).substring(2, 15);
 };
-checkInflight(flightNum, passenger);
-console.log(passenger);
+
+checkInflight(flight, object);
+console.log(object);
+console.log(flight);
 
 // error handling - passing reference object to newPassport function and then try to check-in
 
-newPassport(passenger);
-checkInflight(flightNum, passenger);
-console.log(passenger);
+newPassport(object);
+checkInflight(flight, object); // passing
